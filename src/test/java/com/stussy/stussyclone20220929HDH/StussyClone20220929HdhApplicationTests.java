6680de4498f24bca9a270e89666f5ec07bd187b3
HaDH <gusdk123123@naver.com>
2022-09-29 12:57:58 +0900

    stussy-clonecoding

---
+++ src/test/java/com/stussy/stussyclone20220929HDH/StussyClone20220929HdhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220929HDH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StussyClone20220929HdhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
