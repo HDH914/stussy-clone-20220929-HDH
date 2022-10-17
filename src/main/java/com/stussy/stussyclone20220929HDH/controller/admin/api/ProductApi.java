569c3279218fb812e4ae0781053c1dc434b1b648
HaDH <gusdk123123@naver.com>
2022-10-17 17:15:02 +0900

    stussy-clonecoding study11

--- src/main/java/com/stussy/stussyclone20220929HDH/controller/admin/api/ProductApi.java
+++ src/main/java/com/stussy/stussyclone20220929HDH/controller/admin/api/ProductApi.java
@@ -27,20 +27,24 @@ public class ProductApi {
     @PostMapping("/product")
     public ResponseEntity<?> addProduct(@Validated(ValidationSequence.class) ProductAdditionReqDto productAdditionReqDto, BindingResult bindingResult) throws Exception {
 
-//        String productName = productAdditionReqDto.getName();
-//
-//        for(int i = 0; i < 50; i++) {
-//            if ((i % 4 == 0)){
-//                    productAdditionReqDto.setName(productName + "-" + (i + 1));
-//            }
-//                    productService.addProduct(productAdditionReqDto);
-//        }
+        String productName = productAdditionReqDto.getName();
 
+        for(int i = 0; i < 50; i++) {
+            if (i % 4 == 0){
+                    productAdditionReqDto.setName(productName + "-" + (i + 1));
+            }
+                    productService.addProduct(productAdditionReqDto);
+        }
 
         return ResponseEntity
                 .created(null)
                 .body(new CMRespDto<>(1, "Successfully", null));
     }
+
+//        return ResponseEntity
+//                .created(null)
+//                .body(new CMRespDto<>(1, "Successfully", productService.addProduct(productAdditionReqDto)));
+//    }
     @GetMapping("/products")
     public ResponseEntity<?> getProductList(@RequestParam int pageNumber,
                                             @RequestParam @Nullable String category,
